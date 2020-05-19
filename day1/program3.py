beab29ff5b719973f6d6d70b04929590f0085242
DURGESH PAL <56739907+durgesh2001@users.noreply.github.com>
2020-05-20 01:37:18 +0530

    Create program3.py

---
+++ day1/program3.py
@@ -0,0 +1,7 @@
+a= int(input("enter first number,a: "))
+b= int(input("enter second number,b: "))
+a=a^b
+b=a^b
+a=a^b
+print("after swapping the vaues are:")
+print("a : {}          b : {}".format(a,b))
